98e087658b522aced43dcfe6087e349bee3b31a9
PenguinL <diana1997525@gmail.com>
2021-07-29 00:52:14 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
+#in venv
+#pip install -r req.txt
+pip freeze > req.txt
 #git clone -o skyrim https://github.com/Kovalsky95/skyrim.git
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
+git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
 git pull --all
