bb604255697aebb5b1f650e39ed361eba2816f0f
PenguinLW <diana1997525@gmail.com>
2023-07-04 19:10:05 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
 #in venv
+#source ~/venv/bin/activate
 #pip install -r req.txt
 #pip freeze > req.txt
 chmod u+x *.sh *.py
 
 git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/skyrim
 #git clone -o skyrim https://github.com/PenguinLW/skyrim.git
+#git config --global user.email "diana1997525@gmail.com"
+#git config --global user.name "PenguinLW"
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
@@ -14,3 +17,4 @@ git pull --all
 git add .
 git commit -m PenguinL
 git push --set-upstream skyrim master
+#deactivate
