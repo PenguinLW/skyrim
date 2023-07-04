72a7000f2e71122d717e60e75a8cfc7dd0a32511
PenguinLW <diana1997525@gmail.com>
2023-07-04 18:52:20 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -2,11 +2,14 @@
 #pip install -r req.txt
 #pip freeze > req.txt
 chmod u+x *.sh *.py
+
+git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/skyrim
 #git clone -o skyrim https://github.com/PenguinLW/skyrim.git
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
 git commit -m PenguinL
